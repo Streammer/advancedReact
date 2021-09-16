677908ebff33affb6e93fe0fa58e36e2f97fb398
Streammer <Lukdmitrij@yandex.ru>
2021-09-16 22:57:22 +0300

    postlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import './styles/App.css'
-import PostItem from './components/PostItem'
+import PostList from './components/PostList'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id:1, title:'JavaScript', body: 'Description'},
+    {id:2, title:'JavaScript 1', body: 'Description'},
+    {id:3, title:'JavaScript 2', body: 'Description'}
+  ])
+  const [posts2, setPosts2] = useState([
+    {id:1, title:'Python', body: 'Description'},
+    {id:2, title:'Python 1', body: 'Description'},
+    {id:3, title:'Python 2', body: 'Description'}
+  ])
   return (
     <div className="App">
-      <PostItem/>
+      <PostList posts={posts} title='Посты про JS'/>
+      <PostList posts={posts2} title='Посты про Python'/>
     </div>
   );
 }

9e9385e401b298771f3d9a9e679150aeffa55b40
Streammer <Lukdmitrij@yandex.ru>
2021-09-21 23:08:45 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import './styles/App.css'
 import PostList from './components/PostList'
 import PostForm from "./components/PostForm"
+import MySelect from './components/UI/select/MySelect'
 
 function App() {
   const [posts, setPosts] = useState([
-    {id:1, title:'JavaScript', body: 'Description'},
-    {id:2, title:'JavaScript 1', body: 'Description'},
-    {id:3, title:'JavaScript 2', body: 'Description'}
+    { id: 1, title: 'аа', body: 'вв' },
+    { id: 2, title: 'гг', body: 'ее' },
+    { id: 3, title: 'бб', body: 'сс' }
   ])
 
-const createPost = (newPost) => {
-  setPosts([...posts, newPost])
-}
+  const [selectedSort, setSelectedSort] = useState('')
 
-const removepost = (post) => {
-  setPosts(posts.filter(p=> p.id !== post.id))
-}
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removepost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeСompare(b[sort])))
+  }
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
-      <PostList remove={removepost} posts={posts} title='Посты про JS'/>
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка по'
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' }
+          ]}
+        />
+      </div>
+
+      {posts.length
+        ?
+        <PostList remove={removepost} posts={posts} title='Посты про JS' />
+        :
+        <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
+      }
     </div>
   );
 }
